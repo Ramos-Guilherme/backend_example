9a71b5d9bd1e48447abd07654b91a19e6c913084
Guilherme Ramos <guilherme@raks.com.br>
2022-01-03 20:35:59 -0300

    changed DB config in ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
     type: 'postgres',
-    host: process.env.DB_HOST,
+    url: process.env.DATABASE_URL,
+/*  host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    database: process.env.DATABASE, */
     synchronize: false,
     logging: false,
     entities: [
